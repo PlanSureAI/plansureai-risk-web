d37594c223600c0b65328f486d7d107ed422bcde
TrueValPoperty <williamtylerstreet@gmail.com>
2025-12-31 07:24:54 +0000

    Simplify layout: remove header/footer and add main padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,6 @@
 import "./globals.css";
 import { usePathname } from "next/navigation";
 
-function Logo() {
-  return (
-    <div className="px-8 py-6">
-      {/* remove this <img> if you no longer want the logo anywhere */}
-      <span className="text-sm font-semibold">PlanSureAI</span>
-    </div>
-  );
-}
-
 function Footer() {
   return (
     <footer className="w-full border-t border-neutral-200 px-8 py-4 text-xs text-neutral-500">
@@ -30,14 +21,8 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-white">
-        {/* hide logo on home */}
-        {!isHome && <Logo />}
-
-        <main className="flex-1 flex flex-col">{children}</main>
-
-        {/* hide bottom bar on home */}
-        {!isHome && <Footer />}
+      <body className="min-h-screen bg-slate-50">
+        <main className="py-6 px-4 md:px-8">{children}</main>
       </body>
     </html>
   );
